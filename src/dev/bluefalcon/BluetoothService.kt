fb60dce1706359614e1867421ab5a42b2b48b4a3
Andrew <andrew_reed@hotmail.com>
2020-08-07 13:40:48 +0100

    Added for conflict.

--- src/dev/bluefalcon/BluetoothService.kt
+++ src/dev/bluefalcon/BluetoothService.kt
@@ -5,7 +5,7 @@ import android.bluetooth.BluetoothGattService
 actual class BluetoothService(val service: BluetoothGattService) {
     actual val name: String?
         get() = service.uuid.toString()
-    actual val characteristics: List<BluetoothCharacteristic>
+    actual val chars: List<BluetoothCharacteristic>
         get() = service.characteristics.map {
             BluetoothCharacteristic(it)
         }
