5daf130be016f646e1c8a55ec16f478e358cc218
Andrew Reed <andrew_reed@hotmail.com>
2020-08-19 09:58:37 +0100

    testing

---
+++ src/test/Andrew.kt
@@ -0,0 +1,9 @@
+package test
+
+import Foo
+
+class Andrew: Foo() {
+    override fun bar() {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+}
