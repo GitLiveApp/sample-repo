2ebe61ce3d994e9c094390fa11831933aab016f2
sunny <sunny@git.live>
2021-03-12 16:07:38 +0000

    test

--- src/dev/bluefalcon/RemoteDiffContentProvider.kt
+++ src/dev/bluefalcon/RemoteDiffContentProvider.kt
@@ -19,31 +19,31 @@ export class RemoteDiffContentProvider implements TextDocumentContentProvider {
             const uri = Uri.parse(`gitlive-diff://file/${model.title}?${model.afterPath || model.beforePath}`);
 
             this.models.set(uri.query, model);
-
+            
             // create a new uri with the fragement part replaced by "base"
             const base = uri.with({ fragment: 'base' });
 
             model.placeholder.collect((placeholder: Placeholder)=>{
-            // tell core you opened a webview to signal no need to load anymore
-            project.openDocuments = {['webview']: Uri.parse('')}
-            // open the webview
-            const panel = window.createWebviewPanel(
+                // tell core you opened a webview to signal no need to load anymore
+                project.openDocuments = {['webview']: Uri.parse('')}
+                // open the webview
+                const panel = window.createWebviewPanel(
                     'getRevision', // Identifies the type of the webview. Used internally
-            'Missing working copy', // Title of the panel displayed to the user
-            ViewColumn.One, // Editor column to show the new webview panel in.
-            {enableScripts: true} // Webview options. More on these later.
-            );
+                    'Missing working copy', // Title of the panel displayed to the user
+                    ViewColumn.One, // Editor column to show the new webview panel in.
+                    {enableScripts: true} // Webview options. More on these later.
+                );
 
-            panel.webview.html = getWebviewContent(placeholder);
-            panel.webview.onDidReceiveMessage(
+                panel.webview.html = getWebviewContent(placeholder);
+                panel.webview.onDidReceiveMessage(
                     async () => {
-                await commands.executeCommand('workbench.action.newWindow');
-                //close the webview
-                panel.dispose()
-                return;
-            }
-            );
-        })
+                        await commands.executeCommand('workbench.action.newWindow');
+                        //close the webview
+                        panel.dispose()
+                        return;
+                    }
+                );
+            })            
 
             // the fire method which is used to notify VS Code when a change has happened in a document
             // The provider will then be called again to provide the updated content, assuming the document is still open
@@ -51,14 +51,14 @@ export class RemoteDiffContentProvider implements TextDocumentContentProvider {
             model.afterContent.collect(() => this.onDidChangeEmitter.fire(uri));
             // when remoteDiffModel dies this funciton runs
             model.finally(() => this.models.delete(uri.query));
-
+            
             // wait for succesfull revision retrieval bfore attempting to open diff
             await model.beforeContent.first()
             // Opens the provided resources in the diff editor to compare their contents
             // the gitlive-dii scheme has been registered meaning on each side of the vs code fidd window
             // the returned value of provideTextDocumentContent below will be shown
             await commands.executeCommand('vscode.diff', base, uri, model.title)
-        });
+        });    
 
     }
 
@@ -85,22 +85,46 @@ export class RemoteDiffContentProvider implements TextDocumentContentProvider {
 function getWebviewContent(placeholder: Placeholder) {
     return `<!DOCTYPE html>
     <html lang="en">
-    <head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Clone repository to view teammates diff</title>
-    </head>
-    <body>
-    <h1> ${placeholder.title}</h1>
-    <button onclick=handleButton()>Open Repository in new workspace</button>
-    <script>
-            const vscode = acquireVsCodeApi();
-    function handleButton() {
-        vscode.postMessage({
-            text: 'openRevision'
-        })
-    }
-    </script>
-    </body>
+      <head>
+          <meta charset="UTF-8">
+          <meta name="viewport" content="width=device-width, initial-scale=1.0">
+          <title>Clone repository to view teammates diff</title>
+      </head>
+      <body>
+        <div class="vertical-center">
+          <center>
+            <p> ${placeholder.title}</p>
+            <a href="#" onclick=handleButton()>Open Repository in new   workspace</a>
+          </center> 
+        </div>  
+        <script>
+          const vscode = acquireVsCodeApi();
+          function handleButton() {
+            vscode.postMessage({
+              text: 'openRevision'
+            })
+          }
+        </script>
+        <style>
+          .vertical-center {
+            margin: 0;
+            position: absolute;
+            top: 50%;
+            left: 50%;
+            -ms-transform: translate(-50%, -50%);
+            transform: translate(-50%, -50%);
+          }
+           a {
+             text-decoration: none;
+             color: #3794ff;
+             font-size: 1.3em
+           }
+  
+           p {
+             color: rgba(204, 204, 204, 0.7);
+             font-size: 1.3em
+           }
+         </style>
+      </body>
     </html>`;
 }
