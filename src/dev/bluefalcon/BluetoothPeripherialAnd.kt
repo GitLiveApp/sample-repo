d70637730bef3b8b410d12c6f2716f61f5a31abd
sunny <sunny@git.live>
2021-03-18 17:47:16 +0000

    test

--- src/dev/bluefalcon/BluetoothPeripherialAnd.kt
+++ src/dev/bluefalcon/BluetoothPeripherialAnd.kt
@@ -3,7 +3,6 @@ package dev.bluefalcon
 import android.bluetooth.BluetoothDevice
 
 actual class BluetoothPeripheral(val bluetoothDevice: BluetoothDevice) {
-    val teamhubUser = "test"
     actual val name: String?
         get() = bluetoothDevice.name ?: bluetoothDevice.address
     actual val services: List<BluetoothService>
