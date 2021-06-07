6ffe16ab7029ecfef3d717fbf57d202618c1c82f
sunny <sunny@git.live>
2021-06-07 10:55:35 +0100

    test

--- src/test/kotlin/Andrew.kt
+++ src/test/kotlin/Andrew.kt
@@ -5,11 +5,11 @@ import java.io.Closeable
 
 class dong: Foo() {
     fun bar() {
-        val bf = BluetoothPeripheral()
-        bf.anotherFunc()
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
-        val timHenman = "dsadsadsa"
-        val neverAges = "true story"
-        val gregRusedski = "Is Canadian"
+        val bf = BluetoothPeripheral();
+        bf.anotherFunc();
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.;
+        val timHenman = "dsadsadsa";
+        val neverAges = "true story";
+        val gregRusedski = "Is Canadian";
     }
 }
